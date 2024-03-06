403d2e201abdfd1decfb8a9f2eb3865feff0508d
alexander test <martunov2013@mail.ru>
2024-03-06 15:38:07 +0300

    test added

--- src/test/java/ru/netology/myhw5/vacationservices/VacationServicesTest.java
+++ src/test/java/ru/netology/myhw5/vacationservices/VacationServicesTest.java
@@ -1,4 +1,19 @@
 package ru.netology.myhw5.vacationservices;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class VacationServicesTest {
+    @ParameterizedTest
+    @CsvSource({
+            "10_000,3000,20_000",
+            "100_000,60_000,150_000"
+    })
+    public void testVacationWork(int expected, int income, int expenses, int threshould) {
+
+        int actual = services.calculate(income, expenses, threshould);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
