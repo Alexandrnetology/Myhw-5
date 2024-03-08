0b6f00b3bc9237f7fdd3f947f5925d6cc73a7bd8
alexander test <martunov2013@mail.ru>
2024-03-08 11:52:39 +0300

    Parametrized test added

--- src/test/java/ru/netology/myhw5/vacationservices/VacationServicesTest.java
+++ src/test/java/ru/netology/myhw5/vacationservices/VacationServicesTest.java
@@ -7,15 +7,12 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class VacationServicesTest {
     @ParameterizedTest
     @CsvSource({
-            "10_000,3000,20_000",
-            "100_000,60_000,150_000"
+            "3,10_000,3000,20_000",
+            "2,100_000,60_000,150_000"
     })
-    public void testVacationWork(int income, int expenses, int threshould) {
+    public void testVacationWork(int expected, int income, int expenses, int threshould) {
         VacationServices services = new VacationServices();
-
         int actual = services.calculate(income, expenses, threshould);
-        int expected = 3;
         Assertions.assertEquals(expected, actual);
-
     }
 }
